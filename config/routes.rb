c7d594d34dc19f7096a7c6dc57434f58eb77ada3
mrtkj <45763653+mrtkj@users.noreply.github.com>
2019-11-16 00:28:09 +0900

    project initialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
-  root 'application#hello'
-  # root 'application#goodbye'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
